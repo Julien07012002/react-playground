03705ef136c2b828ccc310f231ece6715791af32
Julien <julientheobald8@gmail.com>
2022-11-15 15:05:59 +0100

    exo5

--- main.js
+++ main.js
@@ -3,14 +3,13 @@ const lastName = 'formateur';
 
 function FirstName(props) {
 
-    /*
-    // Solution avec bonus
+    
     const formatFirstName = (firstName) => {
         return firstName[0].toUpperCase() + firstName.substr(1);
-    }
+    } 
 
     return <span>{formatFirstName(props.text)}</span>
-    */
+    
 
     // Solution sans bonus
     return <span>{props.text[0].toUpperCase() + props.text.substr(1)}</span>
@@ -18,14 +17,14 @@ function FirstName(props) {
 
 function LastName(props) {
 
-    /*
-    // Solution avec bonus
+    
+    
     const formatLastName = (lastName) => {
         return lastName.toUpperCase();
     }
 
     return <span>{formatLastName(props.text)}</span>
-    */
+    
 
     // Solution sans bonus
     return <span className="red-text">{props.text.toUpperCase()}</span>
